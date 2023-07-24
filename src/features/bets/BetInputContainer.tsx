b40a2fdb2bdfd5b347662a48cabbf7372d29b069
AlexDiz <alexdiz88@gmail.com>
2023-07-25 00:43:38 +0200

    add bet input title

--- src/features/bets/BetInputContainer.tsx
+++ src/features/bets/BetInputContainer.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
+import { Dangerous } from '@mui/icons-material';
 import BetInputPlayer from './BetInputPlayer';
 import BetInputLeague from './BetInputLeague';
 import BetInputTeams from './BetInputTeams';
 import BetInputOdds from './BetInputOdds';
+import BetInputTitle from './BetInputTitle';
 
 export default function BetInputContainer(): JSX.Element {
   const [selectedUserId, setSelectedUserId] = useState<string>('');
@@ -11,6 +13,7 @@ export default function BetInputContainer(): JSX.Element {
   const [selectedMatchDay, setSelectedMatchDay] = useState<string>('');
   const [selectedHomeTeamId, setSelectedHomeTeamId] = useState<string>('');
   const [selectedAwayTeamId, setSelectedAwayTeamId] = useState<string>('');
+  const [selectedBetTitle, setSelectedBetTitle] = useState<string>('');
   const [selectedBetOdds, setSelectedBetOdds] = useState<string>('');
   const [selectedBetSize, setSelectedBetSize] = useState<string>('');
 
@@ -29,6 +32,14 @@ export default function BetInputContainer(): JSX.Element {
 
   const handleAwayTeamSelection = (awayTeamId: string): void => {
     setSelectedAwayTeamId(awayTeamId);
+  };
+
+  const handleBetCancel = (): void => {
+    setSelectedBetTitle('');
+  };
+
+  const handleBetTitleSelection = (betTitle: string): void => {
+    setSelectedBetTitle(betTitle);
   };
 
   const handleOddsSelection = (betOdds: string, betSize: string): void => {
@@ -53,9 +64,34 @@ export default function BetInputContainer(): JSX.Element {
           leagueId={selectedLeagueId}
         />
       )}
-      {selectedHomeTeamId && selectedAwayTeamId && (
-        <BetInputOdds onOddsSelect={handleOddsSelection} />
+      {selectedHomeTeamId && selectedAwayTeamId && !selectedBetTitle && (
+        <BetInputTitle onBetTitleSelect={handleBetTitleSelection} />
       )}
+      {selectedBetTitle && (
+        <Box sx={{ my: 2, width: '18.2rem', display: 'flex' }}>
+          <Typography
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: '0.85rem',
+              width: '17rem',
+              height: '2.3rem',
+              border: '1px solid rgba(0, 0, 0, 0.23)',
+              borderRadius: '4px',
+            }}
+          >
+            {selectedBetTitle}
+          </Typography>
+          <IconButton onClick={handleBetCancel}>
+            <Dangerous
+              color="error"
+              sx={{ mt: -1.75, ml: -1.5, fontSize: '3rem' }}
+            />
+          </IconButton>
+        </Box>
+      )}
+      {selectedBetTitle && <BetInputOdds onOddsSelect={handleOddsSelection} />}
     </Box>
   );
 }
