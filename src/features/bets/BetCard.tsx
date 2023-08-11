653b5311e1200aa412defef554898aa6e71abc74
AlexDiz <alexdiz88@gmail.com>
2023-08-11 02:27:57 +0200

    new image path check for deploy

--- src/features/bets/BetCard.tsx
+++ src/features/bets/BetCard.tsx
@@ -71,7 +71,7 @@ export default function BetCard({
           <Avatar
             sx={{ mr: 0.5, ml: 1, width: 25, height: 25 }}
             alt="team_logo"
-            src={`/upload/logo/${awayTeam.fullTitleEn
+            src={`upload/logo/${awayTeam.fullTitleEn
               .toLowerCase()
               .replace(/\s/g, '_')}.png`}
           />
