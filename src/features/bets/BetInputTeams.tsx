9c911b5edab5b65ca82aabc92e0d5e805380fc69
AlexDiz <alexdiz88@gmail.com>
2023-08-11 02:16:08 +0200

    new image path check for deploy

--- src/features/bets/BetInputTeams.tsx
+++ src/features/bets/BetInputTeams.tsx
@@ -151,9 +151,10 @@ export default function BetInputPlayer({
                     <Avatar
                       sx={{ width: 27, height: 27 }}
                       alt="team_logo"
-                      src={`${
-                        process.env.PUBLIC_URL
-                      }/upload/logo/${t.fullTitleEn?.replace(/\s/g, '_')}.png`}
+                      src={`/public/upload/logo/${t.fullTitleEn?.replace(
+                        /\s/g,
+                        '_'
+                      )}.png`}
                     />
 
                     <Typography sx={{ mx: 1, fontSize: '1rem' }}>
