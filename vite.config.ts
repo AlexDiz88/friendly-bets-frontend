5e05a05ee2381ae21c0de2b723c4e7bf396bcf83
AlexDiz <alexdiz88@gmail.com>
2023-10-10 13:04:40 +0200

    add proxy settings to vite config + add logos

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,52 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		open: true,
-	},
-	build: {
-		outDir: 'build',
-		sourcemap: true,
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'src/setupTests',
-		mockReset: true,
-	},
+export default defineConfig(({ command, mode, ssrBuild }) => {
+	if (mode === 'development') {
+		return {
+			plugins: [react()],
+			server: {
+				open: true,
+				proxy: {
+					'/api': {
+						target: 'http://localhost:8080',
+					},
+				},
+			},
+			build: {
+				outDir: 'build',
+				sourcemap: true,
+			},
+			test: {
+				globals: true,
+				environment: 'jsdom',
+				setupFiles: 'src/setupTests',
+				mockReset: true,
+			},
+		};
+	} else {
+		// command === 'build'
+		return {
+			plugins: [react()],
+			server: {
+				open: true,
+				proxy: {
+					'/api': {
+						target: '/',
+					},
+				},
+			},
+			build: {
+				outDir: 'build',
+				sourcemap: true,
+			},
+			base: './',
+			test: {
+				globals: true,
+				environment: 'jsdom',
+				setupFiles: 'src/setupTests',
+				mockReset: true,
+			},
+		};
+	}
 });
