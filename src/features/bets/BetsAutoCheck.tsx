041ea59e766e835b53699602981e9cbd24c77a41
alex lobkov <alexdiz88@gmail.com>
2025-10-23 19:01:26 +0200

    fix AutoCheck page

--- src/features/bets/BetsAutoCheck.tsx
+++ src/features/bets/BetsAutoCheck.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { t } from 'i18next';
 import { useEffect, useMemo, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
@@ -12,11 +12,11 @@ import {
 	showErrorSnackbar,
 	showInfoSnackbar,
 } from '../../components/custom/snackbar/snackbarSlice';
+import useFetchCurrentUser from '../../components/hooks/useFetchCurrentUser';
 import { getActiveSeason, getActiveSeasonId } from '../admin/seasons/seasonsSlice';
 import { selectActiveSeason, selectActiveSeasonId } from '../admin/seasons/selectors';
 import Team from '../admin/teams/types/Team';
 import { getOpenedBets, sendGameResults } from './betsSlice';
-import { externalTeamIdMap } from './externalTeamIdMap';
 import { selectOpenedBets } from './selectors';
 import { ExternalMatchdayData } from './types/ExternalMatchData';
 import GameResult from './types/GameResult';
@@ -44,6 +44,14 @@ export default function BetsAutoCheck(): JSX.Element {
 	const [isSubmitting, setIsSubmitting] = useState(false);
 	const [loading, setLoading] = useState(true);
 	const [loadingError, setLoadingError] = useState(false);
+
+	useFetchCurrentUser();
+
+	// [ADDED] состояние для выбранного турнира
+	const [selectedLeague, setSelectedLeague] = useState(t('all'));
+	const handleLeagueChange = (leagueCode: string): void => {
+		setSelectedLeague(leagueCode);
+	};
 
 	const uniqueGames = useMemo(() => {
 		const set = new Set<string>();
@@ -74,14 +82,12 @@ export default function BetsAutoCheck(): JSX.Element {
 				const updated = { ...prev };
 				delete updated[key];
 
-				// Удаляем ошибку при сбросе счёта
 				setScoreErrors((prevErrors) => {
 					const updatedErrors = { ...prevErrors };
 					delete updatedErrors[key];
 					return updatedErrors;
 				});
 
-				// Сбрасываем валидность при очистке
 				setScoreValidities((prevValidities) => {
 					const updatedValidities = { ...prevValidities };
 					delete updatedValidities[key];
@@ -111,10 +117,8 @@ export default function BetsAutoCheck(): JSX.Element {
 		return data;
 	};
 
-	// Запуск автообновления каждые 5 минут (300000 ms)
 	useEffect(() => {
 		if (!activeSeasonId) return;
-		// fetchExternalResults(); // первый запуск сразу
 		const interval = setInterval(fetchExternalResults, 300_000);
 		return () => clearInterval(interval);
 	}, [activeSeasonId, uniqueGames, inputValues]);
@@ -189,149 +193,192 @@ export default function BetsAutoCheck(): JSX.Element {
 						{t('enterMatchResults')}
 					</Typography>
 
-					{uniqueGames.map((game) => {
-						const isSelected = selectedMatchKey === game.key;
-						const currentInput = inputValues[game.key];
-
-						return (
-							<Box
-								key={game.key}
+					{activeSeason && activeSeason.leagues && (
+						<Box sx={{ display: 'flex', justifyContent: 'center', mt: 1, mb: 2 }}>
+							<Button
 								sx={{
-									px: 2,
-									py: 1,
-									my: 1.5,
-									width: '100%',
-									maxWidth: '26rem',
-									transition: '0.2s',
-									border: '2px solid',
-									borderRadius: 2,
-									bgcolor: isSelected ? '#FFEDD9B9' : '#EDD1C03F',
-									boxShadow: '1px 4px 7px rgba(0, 0, 60, 0.4), 0px 4px 8px rgba(0, 0, 0, 0.7)',
+									borderRadius: 0,
+									borderBottom: selectedLeague === t('all') ? 1 : 0,
+									color: selectedLeague === t('all') ? 'brown' : 'black',
+									fontFamily: "'Exo 2'",
+									px: 0.5,
+									mr: 1,
+									fontWeight: selectedLeague === t('all') ? 'bold' : 'normal',
 								}}
+								onClick={() => handleLeagueChange(t('all'))}
 							>
-								<Box
-									display="flex"
-									justifyContent="space-between"
-									alignItems="center"
-									flexDirection="column"
+								{t('all')}
+							</Button>
+
+							{activeSeason.leagues.map((l) => (
+								<Button
+									key={l.leagueCode}
+									sx={{
+										borderRadius: 0,
+										borderBottom: selectedLeague === l.leagueCode ? 1 : 0,
+										color: selectedLeague === l.leagueCode ? 'brown' : 'black',
+										fontFamily: "'Exo 2'",
+										px: 0.5,
+										mr: 1,
+										fontWeight: selectedLeague === l.leagueCode ? 'bold' : 'normal',
+									}}
+									onClick={() => handleLeagueChange(l.leagueCode)}
 								>
-									<LeagueAvatar
-										leagueCode={game.leagueCode}
-										matchDay={game.matchDay}
-										height={22}
-										sx={{ fontSize: '0.85rem', mb: 0.5 }}
-									/>
-									<TeamsAvatars homeTeam={game.homeTeam} awayTeam={game.awayTeam} height={32} />
-								</Box>
+									{t(`leagueShortName.${l.leagueCode}`)}
+								</Button>
+							))}
+						</Box>
+					)}
 
-								{currentInput && (
-									<Typography
-										sx={{
-											display: 'flex',
-											justifyContent: 'center',
-											mt: 1,
-											color: scoreErrors[game.key] ? '#B00020' : '#1F8300FF',
-											fontWeight: 600,
-											fontSize: '1.05rem',
-										}}
+					{/* фильтр по selectedLeague */}
+					{uniqueGames
+						.filter((g) => selectedLeague === t('all') || g.leagueCode === selectedLeague)
+						.map((game) => {
+							const isSelected = selectedMatchKey === game.key;
+							const currentInput = inputValues[game.key];
+
+							return (
+								<Box
+									key={game.key}
+									sx={{
+										px: 2,
+										py: 1,
+										my: 1.5,
+										width: '100%',
+										maxWidth: '26rem',
+										transition: '0.2s',
+										border: '2px solid',
+										borderRadius: 2,
+										bgcolor: isSelected ? '#FFEDD9B9' : '#EDD1C03F',
+										boxShadow: '1px 4px 7px rgba(0, 0, 60, 0.4), 0px 4px 8px rgba(0, 0, 0, 0.7)',
+									}}
+								>
+									<Box
+										display="flex"
+										justifyContent="space-between"
+										alignItems="center"
+										flexDirection="column"
 									>
-										{t('enteredScore')} - {currentInput.fullTime} ({currentInput.firstTime}
-										{currentInput.overTime ? `, доп. ${currentInput.overTime}` : ''}
-										{currentInput.penalty ? `, пен. ${currentInput.penalty}` : ''})
-									</Typography>
-								)}
+										<LeagueAvatar
+											leagueCode={game.leagueCode}
+											matchDay={game.matchDay}
+											height={22}
+											sx={{ fontSize: '0.85rem', mb: 0.5 }}
+										/>
+										<TeamsAvatars homeTeam={game.homeTeam} awayTeam={game.awayTeam} height={32} />
+									</Box>
 
-								{/* Ошибка счёта матча */}
-								{scoreErrors[game.key] && (
-									<Typography
-										sx={{
-											textAlign: 'center',
-											mt: 0.5,
-											color: '#B00020',
-											fontWeight: 500,
-											fontSize: '0.9rem',
-										}}
-									>
-										{scoreErrors[game.key]}
-									</Typography>
-								)}
-
-								<Box>
-									{!isSelected ? (
-										<Box
+									{currentInput && (
+										<Typography
 											sx={{
 												display: 'flex',
 												justifyContent: 'center',
 												mt: 1,
+												color: scoreErrors[game.key] ? '#B00020' : '#1F8300FF',
+												fontWeight: 600,
+												fontSize: '1.05rem',
 											}}
 										>
-											<CustomButton
-												buttonText={t('btnText.enterScore')}
-												onClick={() => setSelectedMatchKey(game.key)}
-												buttonColor="info"
-												buttonSize="small"
-												textSize="0.75rem"
-												sx={{ mr: 2 }}
-											/>
-											<CustomButton
-												buttonText={t('btnText.resetScore')}
-												onClick={() => handleScoreChange(game.key, null)}
-												buttonColor="warning"
-												buttonSize="small"
-												textSize="0.75rem"
-											/>
-										</Box>
-									) : (
-										<>
-											<ScoreSelector
-												key={game.key}
-												keyId={game.key}
-												initialValue={inputValues[game.key]}
-												onSave={(val) => handleScoreChange(game.key, val)}
-												onValidationChange={(isValid) =>
-													setScoreValidities((prev) => ({ ...prev, [game.key]: isValid }))
-												}
-												onValidationError={(error) =>
-													setScoreErrors((prev) => ({ ...prev, [game.key]: error }))
-												}
-											/>
-											<Box mt={1} display="flex" justifyContent="flex-end">
+											{t('enteredScore')} - {currentInput.fullTime} ({currentInput.firstTime}
+											{currentInput.overTime ? `, доп. ${currentInput.overTime}` : ''}
+											{currentInput.penalty ? `, пен. ${currentInput.penalty}` : ''})
+										</Typography>
+									)}
+
+									{scoreErrors[game.key] && (
+										<Typography
+											sx={{
+												textAlign: 'center',
+												mt: 0.5,
+												color: '#B00020',
+												fontWeight: 500,
+												fontSize: '0.9rem',
+											}}
+										>
+											{scoreErrors[game.key]}
+										</Typography>
+									)}
+
+									<Box>
+										{!isSelected ? (
+											<Box
+												sx={{
+													display: 'flex',
+													justifyContent: 'center',
+													mt: 1,
+												}}
+											>
 												<CustomButton
-													buttonText={t('btnText.reset')}
-													onClick={() => {
-														handleScoreChange(game.key, null);
-														setSelectedMatchKey(null);
-													}}
+													buttonText={t('btnText.enterScore')}
+													onClick={() => setSelectedMatchKey(game.key)}
+													buttonColor="info"
 													buttonSize="small"
-													sx={{ bgcolor: '#DB3833FF', mr: 1 }}
+													textSize="0.75rem"
+													sx={{ mr: 2 }}
 												/>
 												<CustomButton
-													buttonText={t('btnText.accept')}
-													onClick={() => setSelectedMatchKey(null)}
+													buttonText={t('btnText.resetScore')}
+													onClick={() => handleScoreChange(game.key, null)}
+													buttonColor="warning"
 													buttonSize="small"
-													buttonColor="success"
+													textSize="0.75rem"
 												/>
 											</Box>
-										</>
-									)}
+										) : (
+											<>
+												<ScoreSelector
+													key={game.key}
+													keyId={game.key}
+													initialValue={inputValues[game.key]}
+													onSave={(val) => handleScoreChange(game.key, val)}
+													onValidationChange={(isValid) =>
+														setScoreValidities((prev) => ({
+															...prev,
+															[game.key]: isValid,
+														}))
+													}
+													onValidationError={(error) =>
+														setScoreErrors((prev) => ({
+															...prev,
+															[game.key]: error,
+														}))
+													}
+												/>
+												<Box mt={1} display="flex" justifyContent="flex-end">
+													<CustomButton
+														buttonText={t('btnText.reset')}
+														onClick={() => {
+															handleScoreChange(game.key, null);
+															setSelectedMatchKey(null);
+														}}
+														buttonSize="small"
+														sx={{ bgcolor: '#DB3833FF', mr: 1 }}
+													/>
+													<CustomButton
+														buttonText={t('btnText.accept')}
+														onClick={() => setSelectedMatchKey(null)}
+														buttonSize="small"
+														buttonColor="success"
+													/>
+												</Box>
+											</>
+										)}
+									</Box>
 								</Box>
-							</Box>
-						);
-					})}
+							);
+						})}
 
 					{openedBets.length > 0 ? (
-						<>
-							<CustomButton
-								onClick={handleSubmitResults}
-								buttonText={t('sendGameResults')}
-								buttonColor="secondary"
-								disabled={
-									Object.keys(inputValues).length === 0 ||
-									Object.entries(inputValues).some(([key]) => scoreValidities[key] === false)
-								}
-								sx={{ mt: 2 }}
-							/>
-						</>
+						<CustomButton
+							onClick={handleSubmitResults}
+							buttonText={t('sendGameResults')}
+							buttonColor="secondary"
+							disabled={
+								Object.keys(inputValues).length === 0 ||
+								Object.entries(inputValues).some(([key]) => scoreValidities[key] === false)
+							}
+							sx={{ mt: 2 }}
+						/>
 					) : (
 						<Box sx={{ color: '#731212FF', fontWeight: 600, mt: 3 }}>{t('noBets')}</Box>
 					)}
